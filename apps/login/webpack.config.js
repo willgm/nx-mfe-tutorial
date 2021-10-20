5fc7214cb2aa578f6e4e0e0ded0126cc6e049e6f
William Grasel <william.grasel@ifood.com.br>
2021-10-20 11:35:07 -0300

    npx nx g @nrwl/angular:app login --mfe --mfeType=remote --port=4201 --host=dashboard --routing=true

--- apps/login/webpack.config.js
+++ apps/login/webpack.config.js
@@ -27,7 +27,7 @@ sharedMappings.register(
 
 module.exports = {
   output: {
-    uniqueName: 'dashboard',
+    uniqueName: 'login',
     publicPath: 'auto',
   },
   optimization: {
@@ -41,8 +41,10 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      remotes: {
-        login: 'login@http://localhost:4201/remoteEntry.js',
+      name: 'login',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Module': 'apps/login/src/app/remote-entry/entry.module.ts',
       },
       shared: {
         '@angular/core': { singleton: true, strictVersion: true },
