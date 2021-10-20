52e5411ea33cd2697eb2e6559190440622116a71
William Grasel <william.grasel@ifood.com.br>
2021-10-20 12:12:42 -0300

    COPY Dashboard Application content

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { distinctUntilChanged } from 'rxjs/operators';
+
+import { UserService } from '@ng-mfe/shared/data-access-user';
 
 @Component({
   selector: 'ng-mfe-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `
+    <div class="dashboard-nav">Admin Dashboard {{isLoggedIn$ | async | json}}</div>
+    <div *ngIf="isLoggedIn$ | async; else signIn">
+      You are authenticated so you can see this content.
+    </div>
+    <ng-template #signIn><router-outlet></router-outlet></ng-template>
+  `,
+  styles: [``],
 })
-export class AppComponent {
-  title = 'dashboard';
+export class AppComponent implements OnInit {
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit() {
+    this.isLoggedIn$
+      .pipe(distinctUntilChanged())
+      .subscribe(async (loggedIn) => {
+        if (!loggedIn) {
+          this.router.navigateByUrl('login');
+        } else {
+          this.router.navigateByUrl('');
+        }
+      });
+  }
 }
